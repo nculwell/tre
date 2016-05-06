b6f7ff82f1332c8db3dc1579d870238e6031c9c5
Nate C-K <ncmadcity@gmail.com>
2016-05-05 23:59:46 -0500

    More net code.

--- net.c
+++ net.c
@@ -9,12 +9,15 @@
 # include <sys/socket.h>
 #endif
 
-int net_listen() {
+void net_init() {
 #ifdef __WIN32__
   WORD versionWanted = MAKEWORD(1, 1);
   WSADATA wsaData;
   WSAStartup(versionWanted, &wsaData);
 #endif
+}
+
+int net_listen() {
   /*
   struct addrinfo hints;
   struct addrinfo *server_info;
@@ -28,30 +31,80 @@ int net_listen() {
     abort();
   }
   */
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (-1 == sockfd) {
+  int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (-1 == sock_fd) {
     abort();
   }
   //struct sockaddr my_addr;
   struct sockaddr_in addr;
-  int bind_result = bind(sockfd, (struct sockaddr*)&addr, sizeof addr);
+  int bind_result = bind(sock_fd, (struct sockaddr*)&addr, sizeof addr);
   if (0 != bind_result) {
     abort();
   }
-  int listen_result = listen(sockfd, 3);
+  int listen_result = listen(sock_fd, 3);
   if (0 != listen_result) {
     abort();
   }
-  return sockfd;
+  return sock_fd;
 }
 
-int net_accept(int sockfd) {
+int net_accept(int sock_fd) {
   struct sockaddr client_addr;
   socklen_t client_addr_len;
-  int accept_result = accept(sockfd, &client_addr, &client_addr_len);
-  if (0 != accept_result) {
+  int accepted_fd = accept(sock_fd, &client_addr, &client_addr_len);
+  if (-1 == accepted_fd) {
+    abort();
+  }
+  int close_result = net_close(sock_fd);
+  if (-1 == close_result) {
+    abort();
+  }
+  return accepted_fd;
+}
+
+int net_recv(int sock_fd) {
+  // TODO: MSG_DONTWAIT flag for nonblocking
+  char buf[BUFSIZ];
+  int buf_len = BUFSIZ;
+  int recv_len = recv(sock_fd, buf, buf_len, 0);
+  if (-1 == recv_len) {
+    if (EAGAIN == errno || EWOULDBLOCK == errno) {
+      return -1;
+    } else {
+      abort();
+    }
+  }
+  if (0 == recv_len) {
+    // Connection closed.
+    return 0;
+  }
+  return recv_len;
+}
+
+int net_send(int sock_fd, char* buf, int buf_len) {
+  // TODO: MSG_DONTWAIT flag for nonblocking
+  int send_result = send(sock_fd, buf, buf_len, 0);
+  if (-1 == send_result) {
+    if (EAGAIN == errno || EWOULDBLOCK == errno) {
+      return -1;
+    } else {
+      abort();
+    }
+  }
+  if (send_result < buf_len) {
+    // TODO: If not all data is sent, loop, or queue the rest for later.
+    fprintf(stderr, "Send truncated.\n");
     abort();
   }
   return 1;
+}
+
+int net_close(int sock_fd) {
+  return 
+#ifdef _WIN32
+    closesocket(sock_fd);
+#else
+    close(sock_fd);
+#endif
 }
 
